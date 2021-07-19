911eb26fc1a6287ce5d0683f74e23bac05078bc9
Akshansh <s.akshansh01@gmail.com>
2021-07-19 21:44:39 +0530

    Day 14 completed

---
+++ Day 14/script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    document.querySelector(".box").style.backgroundColor = "red";
+  }
+
+function hover() {
+    document.querySelector(".box").style.backgroundColor = "blue";
+}
+
+function justChill() {
+    document.querySelector(".box").style.backgroundColor = "beige";
+}
